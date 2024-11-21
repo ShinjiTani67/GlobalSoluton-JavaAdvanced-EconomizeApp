73c437855a53c714990479013ae931b68ea14584
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-11-21 20:40:45 -0300

    update controller

--- src/main/java/controller/HouseController.java
+++ src/main/java/controller/HouseController.java
@@ -4,10 +4,13 @@ package controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import repository.HouseRepository;
 
 @RestController
 @RequestMapping("/api/casa")
 @Tag(name = "Casa", description = "Gerencia os dados da casa")
 public class HouseController {
-
+    public HouseController(HouseRepository houseRepository) {
+        
+    }
 }
--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -18,7 +18,7 @@ public class UserController {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
-    public UserController(UserRepository userRepository, UserMapper userMapper, UserMapper userMapper1) {
+    public UserController(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
     }
