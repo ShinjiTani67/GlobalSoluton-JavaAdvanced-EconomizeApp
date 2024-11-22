dffa49aec19a14de7fd650678deae5b9bbef5163
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-11-22 15:56:56 -0300

    update house mapper

--- src/main/java/mapper/HouseMapper.java
+++ src/main/java/mapper/HouseMapper.java
@@ -1,11 +1,12 @@
 package mapper;
 
 import dto.HouseDto;
+import jakarta.validation.Valid;
 import model.House;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface HouseMapper {
     HouseDto toDto(House house);
-    House toEntity(House houseDto);
+    House toEntity(@Valid HouseDto houseDto);
 }
